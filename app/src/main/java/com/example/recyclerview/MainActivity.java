785240ab1996a8bb131725612b0cdb8ae6571a2d
YJONG3 <yujinn1995@gmail.com>
2020-10-05 11:48:19 -0400

    Version 1

--- app/src/main/java/com/example/recyclerview/MainActivity.java
+++ app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.recyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    RecyclerView recyclerView;
+
+    String s1[], s2[];
+    int images[] = {R.drawable.sandwiches, R.drawable.salad, R.drawable.soup, R.drawable.mousse, R.drawable.tilapia, R.drawable.cake, R.drawable.applepie, R.drawable.friedrice,
+            R.drawable.pasta, R.drawable.burger };
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+        s1 = getResources().getStringArray(R.array.recipe);
+        s2 = getResources().getStringArray(R.array.description);
     }
 }
