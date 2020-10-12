0ab3a36619942487a767def79dfde594aa29faf5
YJONG3 <yujinn1995@gmail.com>
2020-10-12 11:37:53 -0400

    A5.2

--- app/src/main/java/com/example/recyclerview/splash.java
+++ app/src/main/java/com/example/recyclerview/splash.java
@@ -3,6 +3,7 @@ package com.example.recyclerview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import java.util.Timer;
@@ -10,10 +11,19 @@ import java.util.TimerTask;
 
 public class splash extends AppCompatActivity {
 
+        MediaPlayer sound;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+
+        sound = MediaPlayer.create(this, R.raw.rain);
+        sound.start();
+
+
         TimerTask task=new TimerTask() {
             @Override
             public void run() {
@@ -26,4 +36,13 @@ public class splash extends AppCompatActivity {
         opening.schedule(task,5000);
 
     }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        sound.release();
+        finish();
+
+    }
+
 }
